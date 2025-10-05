6c5f6bdf26fc809b5bff0716a74126079f156293
SIPROSAPP <gusgabarenasok@gmail.com>
2025-10-05 01:39:00 -0300

    Agregar pagina principal que redirige al admin

--- backend/urls.py
+++ backend/urls.py
@@ -4,9 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from core.views import health_check
+from core.views import health_check, home
 
 urlpatterns = [
+    path('', home, name='home'),  # Página principal redirige al admin
     path('admin/', admin.site.urls),
     path('api/', include("core.urls")),  # 👈 importante
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
